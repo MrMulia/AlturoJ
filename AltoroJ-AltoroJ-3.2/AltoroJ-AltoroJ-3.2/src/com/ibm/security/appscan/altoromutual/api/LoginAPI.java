3b37a1fa645de5d150deaa16cea5d6172dba56de
BrandonBakerCS <109840185+BrandonBakerCS@users.noreply.github.com>
2024-04-17 21:43:10 -0700

    Added a check to see if the userExists and changed the error response

--- AltoroJ-AltoroJ-3.2/AltoroJ-AltoroJ-3.2/src/com/ibm/security/appscan/altoromutual/api/LoginAPI.java
+++ AltoroJ-AltoroJ-3.2/AltoroJ-AltoroJ-3.2/src/com/ibm/security/appscan/altoromutual/api/LoginAPI.java
@@ -17,67 +17,71 @@ import com.ibm.security.appscan.altoromutual.util.DBUtil;
 import com.ibm.security.appscan.altoromutual.util.OperationsUtil;
 
 @Path("/login")
-public class LoginAPI{
+public class LoginAPI {
 
-	@GET
-	public Response checkLogin(@Context HttpServletRequest request) throws JSONException {
-		JSONObject myJson = new JSONObject();
-		myJson.put("loggedin", "true");
-		return Response.status(Response.Status.OK).entity(myJson.toString()).type(MediaType.APPLICATION_JSON_TYPE).build();
-	}
+    @GET
+    public Response checkLogin(@Context HttpServletRequest request) throws JSONException {
+        JSONObject myJson = new JSONObject();
+        myJson.put("loggedin", "true");
+        return Response.status(Response.Status.OK).entity(myJson.toString()).type(MediaType.APPLICATION_JSON_TYPE).build();
+    }
 
-	@POST
-	@PermitAll
-	public Response login(String bodyJSON, @Context HttpServletRequest request) throws JSONException {
+    @POST
+    @PermitAll
+    public Response login(String bodyJSON, @Context HttpServletRequest request) throws JSONException {
+        JSONObject myJson = new JSONObject();
+        try {
+            myJson = new JSONObject(bodyJSON);
+        } catch (Exception e) {
+            // e.printStackTrace();
+            myJson.clear();
+            myJson.put("error", "body is not JSON");
+            return Response.status(Response.Status.BAD_REQUEST).entity(myJson.toString()).build();
+        }
 
-		JSONObject myJson = new JSONObject();
-		try {
-			myJson =new JSONObject(bodyJSON);
-		} catch (Exception e) {
-			// e.printStackTrace();
-			myJson.clear();
-			myJson.put("error", "body is not JSON");
-			return Response.status(Response.Status.BAD_REQUEST).entity(myJson.toString()).build();
-		}
+        // Check username and password parameters are there
+        if (!(myJson.containsKey("username") && myJson.containsKey("password"))) {
+            myJson.clear();
+            myJson.put("error", "username or password parameter missing");
+            return Response.status(Response.Status.BAD_REQUEST).entity(myJson.toString()).build();
+        }
 
-		// Check username and password parameters are there
-		if (!(myJson.containsKey("username") && myJson.containsKey("password"))) {
-			myJson.clear();
-			myJson.put("error", "username or password parameter missing");
-			return Response.status(Response.Status.BAD_REQUEST).entity(myJson.toString()).build();
-		}
-
-		String username, password;
+        String username, password;
 		username = myJson.get("username").toString().toLowerCase();
 		password = myJson.get("password").toString().toLowerCase();
-
-		myJson.clear();
+        myJson.clear();
 		
-		try {
-			if (!DBUtil.isValidUser(username, password)) {
-				throw new InvalidParameterException(
-						"We're sorry, but this username or password was not found in our system.");
-			}
-		} catch (Exception e) {
-			if (e instanceof InvalidParameterException)
-				System.out.println("Invalid user error: " + e.getLocalizedMessage());
-			
-			myJson.put("error", e.getLocalizedMessage());
-			return Response.status(Response.Status.BAD_REQUEST).entity(myJson.toString()).build();
-		}
+		// added check to see if the username exists
+        // User enumeration vulnerability: differentiating between non-existing user and wrong password
+        // This will show in the error reponse
+        try {
+            if (!DBUtil.isValidUser(username, password)) {
+                boolean userExists = DBUtil.userExists(username); // created new method 'userExists' in DBUtil
+                if (!userExists) {
+                    myJson.put("error", "Username not found: " + username);
+                } else {
+                    myJson.put("error", "Incorrect password for user: " + username);
+                    myJson.put("username", username);
+                    myJson.put("password", password);
+                }
+                return Response.status(Response.Status.BAD_REQUEST).entity(myJson.toString()).build();
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid user error: " + e.getLocalizedMessage());
+            myJson.put("error", e.getLocalizedMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity(myJson.toString()).build();
+        }
 
-		try {
-			myJson.put("success", username + " is now logged in");
-			
-			//Generate a very basic auth token      			
-			String authToken = Base64.encodeBase64String(username.getBytes()) +":"+ Base64.encodeBase64String(password.getBytes()) +":"+OperationsUtil.makeRandomString();
-			
-			myJson.put("Authorization",Base64.encodeBase64String(authToken.getBytes()));
-			return Response.status(Response.Status.OK).entity(myJson.toString()).type(MediaType.APPLICATION_JSON_TYPE).build();
-		} catch (Exception ex) {
-			myJson.put("failed", "Unexpected error occured. Please try again.");
-			myJson.put("error", ex.getLocalizedMessage());
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(myJson.toString()).build();
-		}
-	}
+        try {
+            myJson.put("success", username + " is now logged in");
+            // Generate a very basic auth token
+            String authToken = Base64.encodeBase64String(username.getBytes()) +":"+ Base64.encodeBase64String(password.getBytes()) +":"+OperationsUtil.makeRandomString();
+            myJson.put("Authorization", Base64.encodeBase64String(authToken.getBytes()));
+            return Response.status(Response.Status.OK).entity(myJson.toString()).type(MediaType.APPLICATION_JSON_TYPE).build();
+        } catch (Exception ex) {
+            myJson.put("failed", "Unexpected error occurred. Please try again.");
+            myJson.put("error", ex.getLocalizedMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(myJson.toString()).build();
+        }
+    }
 }
